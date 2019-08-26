9b426a3d308d7cfdc9c18e94b6c6f003a5dfdb2b
GUSTAVO  VILLELA RUBIA <17.01736-0@maua.br>
2019-08-26 13:10:07 -0300

    Aula 14

---
+++ Aula 14/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import auxiliar_database.UsuariosDAO;
+import model.Usuario;
+import utilities.Constants;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        UsuariosDAO usuariosDAO;
+        usuariosDAO = new UsuariosDAO(Constants.URL_MEU_BANCO);
+        List<Usuario>meusUsuarios = usuariosDAO.getAllUsuario();
+        for (Usuario user: meusUsuarios) {
+            System.out.print("ID: " + user.id);
+            System.out.println("Nome: " + user.nome);
+            {
+        }
+        }
+    }
+}
